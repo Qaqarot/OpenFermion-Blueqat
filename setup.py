c006973650d763bb39ceb2701c6dd9fff5b3d6f6
gyu-don <takumi.kt+git@gmail.com>
2019-02-05 16:24:00 +0900

    Add install_requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import setuptools
 
-with open("README.rst", "r") as f:
+with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open("openfermionblueqat/_version.py", "r") as f:
+with open("openfermionblueqat/_version.py", "r", encoding="utf-8") as f:
     exec(f.read())
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = list(map(str.strip, f))
 
 setuptools.setup(
     name = "openfermionblueqat",
@@ -17,6 +20,7 @@ setuptools.setup(
     url="https://github.com/mdrft/OpenFermion-Blueqat",
     license="Apache 2",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
